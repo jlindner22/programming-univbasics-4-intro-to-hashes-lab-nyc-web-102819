657dd918c33c7934f48d1b99df1d9dc12a432548
Jen Lindner <jenlindner22@gmail.com>
2019-10-16 22:28:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def update_counting_hash(hash, key)
 if hash[:rambutan]
  puts "Rambutan present!"
 else
-puts "No rambutan."
+puts key[1]
 end
 end
   # if the provided key is not present in the hash, add it and assign it to the value of 1
