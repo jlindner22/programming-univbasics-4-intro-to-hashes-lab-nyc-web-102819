9fb4953e72b3560a33e6b53378b5830f987d3074
Jen Lindner <jenlindner22@gmail.com>
2019-10-16 23:39:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ else
  key[value]
 end
 end
-end
+
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 
